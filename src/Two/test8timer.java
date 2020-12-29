ed67e80740d5ba6f8ebee66bccccdb54e9ba093c
chenhaoya <1627785181@qq.com>
2020-12-29 10:35:24 +0800

    22

--- src/Two/test8timer.java
+++ src/Two/test8timer.java
@@ -1,5 +1,44 @@
 package Two;
 
-public class test8timer {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+/**
+ * 目的：设置一个标签显示数字，从10递减到0停止
+ * */
+public class test8timer {
+    private JFrame jFrame=new JFrame("timer");
+    private JLabel jLabel=new JLabel("10");
+    private Timer timer;
+    public void init(){
+        Container container=jFrame.getContentPane();//申请附属容器并和窗口绑定
+        container.setLayout(new BorderLayout());//边界布局管理器
+        //设置标签中文字为水平居中对其
+        jLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        container.add(jLabel);
+        timer=new Timer(1000, new ActionListener() {//1000毫秒
+            //每隔一秒，触发一个事件，且会被Action Listener接口监听到，并调用接口中的方法对事件处理
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println(e.getSource()+"定时器");
+//                System.out.println(e.getActionCommand()+"定时器");
+//                System.out.println(e.getSource().);
+                //改变标签中的数字，到0为止
+                int num=Integer.parseInt(jLabel.getText());//解析字符串中的整数
+                if (num>0)
+                    jLabel.setText(--num+"");
+                else timer.stop();
+            }
+        });
+        timer.start();
+        jFrame.setBounds(500,400,500,400);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jFrame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new test8timer().init();
+    }
 }
---
+++ src/Two/test9eveent1.java
@@ -0,0 +1,64 @@
+package Two;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class test9eveent1 {
+    private Frame frame=new Frame("event1");
+    private TextArea textArea=new TextArea(5,30);
+    private Panel panel=new Panel();//次级容器
+    private Button button=new Button("click1");
+    private Button button2=new Button("click2");
+    public void init(){
+        /**        创建监听器
+        * 一个组件可被多个监听器监听
+        * 一个监听器也可以监听多个组件
+        * */
+        Listener1 l1=new Listener1();
+        Listener2 l2=new Listener2();
+        //给组件添加监听
+        button.addActionListener(l1);
+        button.addActionListener(l2);
+        button2.addActionListener(l1);
+        frame.add(textArea);
+        panel.add(button);
+        panel.add(button2);
+        frame.add(panel,BorderLayout.SOUTH);
+        frame.setBounds(500,400,500,400);
+        frame.setVisible(true);
+
+    }
+
+    public static void main(String[] args) {
+        new test9eveent1().init();
+    }
+    class Listener1 implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            textArea.append("***监听器1"+e.getActionCommand()+"被点击\r\n");//在文本域追加信息
+            if(e.getSource()==button) {
+                System.out.println("button被点击");
+                textArea.append("button被点击\n");
+            }
+            else if(e.getSource()==button2) {
+                System.out.println("button2被点击");
+                textArea.append("button2被点击\n");
+            }
+        }
+    }
+    class Listener2 implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            textArea.append("***监听器2"+e.getActionCommand()+"被点击\r\n");
+            if(e.getSource()==button) {
+                System.out.println("button被点击");
+                textArea.append("button被点击\n");
+            }
+            else if(e.getSource()==button2) {
+                System.out.println("button2被点击");
+                textArea.append("button2被点击\n");
+            }
+        }
+    }
+}
